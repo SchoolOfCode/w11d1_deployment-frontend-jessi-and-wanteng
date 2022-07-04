685ee16fd3e6aa84de0ef8913ff8d6c8d329b1ca
wanteng <wantengteoh@gmail.com>
2022-07-04 15:47:31 +0100

    testing to see each push updates with the deployment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to deploy with Netlify
         </a>
       </header>
     </div>
