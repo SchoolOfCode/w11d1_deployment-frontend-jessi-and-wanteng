7b7d855645e61242f5ea1b80a1b434851287ecaf
wanteng <wantengteoh@gmail.com>
2022-07-04 15:54:46 +0100

    deployment is locked on netifly, test to see whether auto deployment works with each push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to deploy with Netlify
+          Learn to deploy with Netlify123
         </a>
       </header>
     </div>
