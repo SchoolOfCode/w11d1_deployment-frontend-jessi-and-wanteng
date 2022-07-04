dc5676c9224435bfffb441e1a45a1474080a387f
wanteng <wantengteoh@gmail.com>
2022-07-04 16:02:14 +0100

    testing how long auto-deploy react to each git push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to deploy with Netlify123
+          Learn to deploy with Netlify456
         </a>
       </header>
     </div>
